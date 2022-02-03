21181f476f7c70f13212fdb08fa4a10a6533f53f
chantelle mckenzie <mrspasceri@hotmail.com>
2022-02-02 21:55:56 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <header>
+            <div className='wrapper'>
+              <h1>Fun Food Friends</h1>
+              
+            </div>
+        </header>
+        <div className='container'>
+          <section className='add-item'>
+              <form>
+                <input type="text" name="username" placeholder="What's your name?" />
+                <input type="text" name="currentItem" placeholder="What are you bringing?" />
+                <button>Add Item</button>
+              </form>
+          </section>
+          <section className='display-item'>
+            <div className='wrapper'>
+              <ul>
+              </ul>
+            </div>
+          </section>
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
